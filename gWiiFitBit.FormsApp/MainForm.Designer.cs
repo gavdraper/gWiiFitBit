ae1a1e7bd8c5733e85530cf66f6795fbdbc5d748
Gavin Draper <gavd83@hotmail.co.uk>
2012-11-14 19:41:08 +0000

    Initial Commit

---
+++ gWiiFitBit.FormsApp/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace gWiiFitBit.FormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Location = new System.Drawing.Point(100, 118);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(35, 13);
+            this.lblWeight.TabIndex = 0;
+            this.lblWeight.Text = "label1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 354);
+            this.Controls.Add(this.lblWeight);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWeight;
+    }
+}
+
---
+++ gWiiFitBit.FormsApp/MainForm.cs
@@ -0,0 +1,105 @@
+﻿using gWiiFitBit.FormsApp.FitBit;
+using gWiiFitBit.Lib;
+using RestSharp;
+using RestSharp.Authenticators;
+using RestSharp.Contrib;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Web;
+using System.Windows.Forms;
+
+namespace gWiiFitBit.FormsApp
+{
+    public partial class MainForm : Form
+    {
+        Thread wiiBoardThread;
+        WiiBoard wiiBoard;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            /*    wiiBoard = new WiiBoard();
+                wiiBoard.OnWeightChanged += OnWeightChanged;
+                wiiBoardThread = new Thread(() =>
+                {
+                    wiiBoard.Connect();
+                });
+                wiiBoardThread.Start();*/
+            UploadWeightToFitBit(new Weight(71));
+        }
+
+        private void OnWeightChanged(Weight weight)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                lblWeight.Text = weight.ToString();
+            });
+            UploadWeightToFitBit(weight);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (wiiBoardThread.IsAlive) wiiBoardThread.Abort();
+            wiiBoard.Disconnect();
+        }
+
+        private void UploadWeightToFitBit(Weight weight)
+        {/*
+            Authenticator a = new Authenticator(
+               System.Configuration.ConfigurationManager.AppSettings["ConsumerKey"],
+               System.Configuration.ConfigurationManager.AppSettings["ConsumerSecret"],
+               "http://api.fitbit.com/oauth/request_token",
+
+               "http://api.fitbit.com/oauth/access_token",
+               "http://www.fitbit.com/oauth/authorize"
+               );
+            a.GetAuthUrlToken();*/
+            //Pin : pm4jaad5migqs153dddqi0p3o4
+
+            var consumerKey = System.Configuration.ConfigurationManager.AppSettings["ConsumerKey"];
+            var consumerSecret = System.Configuration.ConfigurationManager.AppSettings["ConsumerSecret"];
+            var OAuthToken = System.Configuration.ConfigurationManager.AppSettings["OAuthToken"];
+            var OAuthTokenSecret = System.Configuration.ConfigurationManager.AppSettings["OAuthTokenSecret"];
+            /*
+            var client = new RestClient("http://api.fitbit.com/");
+            client.Authenticator = OAuth1Authenticator.ForRequestToken(consumerKey, consumerSecret);
+            var request = new RestRequest("oauth/request_token", Method.POST);
+            var response = client.Execute(request);
+            var qs = RestSharp.Contrib.HttpUtility.ParseQueryString(response.Content);
+            var oauth_token = qs["oauth_token"];
+            var oauth_token_secret = qs["oauth_token_secret"];
+            request = new RestRequest("oauth/authorize");
+            request.AddParameter("oauth_token", oauth_token);
+            var url = client.BuildUri(request).ToString();
+            Process.Start(url);
+
+            var verifier = "ep5pmianh5lg1n54ef9hhf9a4v"; // <-- Breakpoint here (set verifier in debugger)
+            request = new RestRequest("oauth/access_token", Method.POST);
+            client.Authenticator = OAuth1Authenticator.ForAccessToken(
+                consumerKey, consumerSecret, oauth_token, oauth_token_secret, verifier
+            );
+            response = client.Execute(request);
+
+            qs = RestSharp.Contrib.HttpUtility.ParseQueryString(response.Content);
+            oauth_token = qs["oauth_token"];
+            */
+            //Perform Actions
+            var client = new RestClient("http://api.fitbit.com/");
+            client.Authenticator = OAuth1Authenticator.ForRequestToken(consumerKey, consumerSecret);
+            var request = new RestRequest("1/user/-/body/log/weight.json", Method.POST);
+            request.AddParameter("weight", weight.TotalStone.ToString());
+            request.AddParameter("date", DateTime.UtcNow.ToString("yyyy-MM-dd"));
+        //    request.AddParameter("time", DateTime.UtcNow.ToString("HH:mm:ss"));
+            client.Authenticator = OAuth1Authenticator.ForProtectedResource(
+                consumerKey, consumerSecret, OAuthToken, OAuthTokenSecret);
+            var response = client.Execute(request);
+        }
+
+
+    }
+}
